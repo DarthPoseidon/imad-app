63a8b5a94fe81ea3ae6f8c7750df5898cb501758
DarthPoseidon <36617752+DarthPoseidon@users.noreply.github.com>
2018-02-23 22:50:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/kandha.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'kandha.html'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
